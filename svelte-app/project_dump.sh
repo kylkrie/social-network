af3e978b859cd955e5be49e158827c52bd4fa88b
Kyle Smith <kylkrie@gmail.com>
2024-08-30 10:18:02 -0400

    docker compose fixes

--- svelte-app/project_dump.sh
+++ svelte-app/project_dump.sh
@@ -7,7 +7,7 @@ src_directory="src"
 >"$output_file"
 
 # Loop through all .ts and .svelte files in src directory, excluding node_modules
-find "$src_directory" -type f \( -name "*.ts" -o -name "*.svelte" \) -not -path "*/node_modules/*" | while read file; do
+find "$src_directory" -type f \( -name "*.ts" -o -name "*.svelte" -o -name "*.css" \) -not -path "*/node_modules/*" | while read file; do
   echo "// $file" >>"$output_file"
   cat "$file" >>"$output_file"
   echo -e "\n" >>"$output_file"
